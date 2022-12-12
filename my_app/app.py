7bbd462830588c818a3c747142486359026cdd90
Danya <s02210352@gse.cs.msu.ru>
2022-12-12 22:34:43 +0300

    final

--- my_app/app.py
+++ my_app/app.py
@@ -36,14 +36,15 @@ app_ui = ui.page_fluid(
             ui.output_plot("prob_neg_bin"),
 
             ui.h3("Функция распределения"),
-            ui.h5("\( F(k; r, p) = I_p(k + 1, r) \)"),
+            ui.h5("\( F(k) = I_p(r, k + 1) \)"),
+            ui.output_plot("prob_neg_bin2"),
 
             ui.h3("Характеристики:"),
             ui.tags.ul(
                 {"style":"list-style-type:circle;font-size: 20px"},
                 ui.tags.li("\(Математическое \ ожиданние: \\frac{rq}{p} \)"),
                 ui.tags.li("\(Дисперсия: \\frac{rq}{p^2} \)"),
-                ui.tags.li("\(Мода: [\\frac{(r-1)q}{p}] \ если \ r > 1, \ 0 \ если \ r \leq \ 1\)"),
+                ui.tags.li("\(Мода: [\\frac{(r-1)q}{p}] \ если \ r > 1; \ 0 \ если \ r \leq \ 1\)"),
                 ui.tags.li("\(Коэфициент \ ассиметрии: \\frac{2-p}{\\sqrt{rq}} \)"),
                 ui.tags.li("\(Коэфициент \ эксцесса: \\frac{6}{r} + \\frac{p^2}{rq} \)"),
             ),
@@ -57,6 +58,20 @@ app_ui = ui.page_fluid(
     ),
     
 )
+
+def rectangle_method(f, a, b, N, r, k):
+    ans = 0
+    h = (b - a) / N
+    mid = (2 * a + h) / 2
+    for i in range(N):
+        ans += f(mid, r, k + 1)
+        mid += h
+    
+    ans *= h
+    return ans
+
+def f(t, a, b):
+    return t ** (a - 1) * (1 - t) ** (b - 1)
 
 def fac(n):
     factorial = 1
@@ -79,16 +94,16 @@ def server(input, output, session):
         p = input.slide1()
         r = input.slide2()
 
-        plt.xlabel("x")
+        plt.xlabel("k")
         plt.ylabel("p")
         plt.grid()
-        x = [0] * 10
-        y = [0] * 10
-        for i in range (0 , 10):
+        x = np.zeros(25)
+        y = np.zeros(25)
+        for i in range (0 , 25):
             x[i] = i
         x_1 = 0
         factorial = 1
-        for i in range (1, 10):
+        for i in range (1, 25):
             x_1 = combinations(x[i] + r - 1, x[i])
             y[i] = x_1 * (p ** r) * ((1 - p) ** x[i])
             ax.plot([x[i], x[i]], [0, y[i]], 'blue')
@@ -97,7 +112,25 @@ def server(input, output, session):
         
         return fig
     
-    
+    @output
+    @render.plot
+    def prob_neg_bin2():
+        fig, ax = plt.subplots()
+        p = input.slide1()
+        r = input.slide2()
+
+        plt.xlabel("k")
+        plt.ylabel("p")
+        plt.grid()
+        x = np.zeros(25)
+        y = np.zeros(25)
+        for i in range (0 , 25):
+            x[i] = i
+        for i in range(25):
+            y[i] = rectangle_method(f, 0, p, 100, r, x[i]) / rectangle_method(f, 0, 1, 100, r, x[i])
+        
+        ax.plot(x, y)
+        return fig
 
     
 
